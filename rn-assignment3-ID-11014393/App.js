89b793e79f29d58b6ce9bd1a14eb5dd41423f55d
boss-EL <edatsomor010@st.ug.edu.gh>
2024-06-01 00:14:57 +0000

    completed Nav.js

--- rn-assignment3-ID-11014393/App.js
+++ rn-assignment3-ID-11014393/App.js
@@ -1,17 +1,27 @@
+// App.js
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
+import Nav from './Nav';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Nav />
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#F7F0E8',
+  },
   container: {
-   
+    flex: 1,
+    backgroundColor: '#F7F0E8',
   },
 });
---
+++ rn-assignment3-ID-11014393/Nav.js
@@ -0,0 +1,75 @@
+// Nav.js
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const Nav = () => {
+  return (
+    <View style={styles.navContainer}>
+      <View style={styles.navText}>
+        <View style={styles.upperTitle}>
+          <Text style={styles.upperTitleText}>
+            Hello, Devs
+          </Text>
+        </View>
+        <View style={styles.lowerTitle}>
+          <Text style={styles.lowerTitleText}>
+            14 tasks today
+          </Text>
+        </View>
+      </View>
+      <View style={styles.navImageContainer}>
+        <Image source={require('./assets/avatar.png')} style={styles.navImage} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  navContainer: {
+    flexDirection: 'row',
+    height: 52,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+  navText: {
+    flexDirection: 'column',
+  },
+  upperTitle: {
+    height: 37,
+  },
+  upperTitleText: {
+    fontFamily: 'Lato',
+    fontSize: 32,
+    fontWeight: '700',
+    lineHeight: 38.4,
+    textAlign: 'left',
+    color: '#000000',
+  },
+  lowerTitle: {
+    height: 15,
+  },
+  lowerTitleText: {
+    fontFamily: 'Lato',
+    fontSize: 12,
+    fontWeight: '500',
+    lineHeight: 14.4,
+    textAlign: 'left',
+  },
+  navImageContainer: {
+    width: 52,
+    height: 52,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 26,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  navImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+});
+
+export default Nav;
